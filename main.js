0eaacbc16ab41cb72a27da5fb16daa0806da7c54
Snaida Kabalika <snaida.kabalika@student.moringaschool.com>
2022-05-15 16:01:14 +0300

    add variables to my js file

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ var correct = document.getElementById('correct')
 var wrong = document.querySelectorAll("#p1")
 
 
+var submit= document.getElementById("quiz-app")
+var correct= document.getElementById("correct")
+var wrong= document.querySelectorAll("#p1")
+
