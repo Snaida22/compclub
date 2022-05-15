de02a93d4ea756e1ca8331414aca8f97c4fea2f2
Snaida Kabalika <snaida.kabalika@student.moringaschool.com>
2022-05-15 16:04:57 +0300

    add function to prevent default

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ var submit= document.getElementById("quiz-app")
 var correct= document.getElementById("correct")
 var wrong= document.querySelectorAll("#p1")
 
+submit.addEventListener('submit', function result(e){
+  e.preventDefault()
+  var marks = 0;
+  var q1 = document.quiz.question1.value;
+  var q2 = document.quiz.question2.value;
+  var q3 = document.quiz.question3.value;
+  var q4 = document.quiz.question4.value;
+  var q5 = document.quiz.question5.value;
+  var q6 = document.quiz.question6.value;
